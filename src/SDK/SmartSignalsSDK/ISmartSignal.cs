7e3f988ebee8a01f99fde3c8cfb0935cb6b8906e
Eran Gonen <erangon@yahoo.com>
2017-11-30 09:12:35 +0200

    Fixed CR comments

--- src/SDK/SmartSignalsSDK/ISmartSignal.cs
+++ src/SDK/SmartSignalsSDK/ISmartSignal.cs
@@ -1,9 +1,4 @@
-﻿//-----------------------------------------------------------------------
-// <copyright company="Microsoft Corporation">
-//        Copyright (c) Microsoft Corporation.  All rights reserved.
-// </copyright>
-//-----------------------------------------------------------------------
-namespace Microsoft.Azure.Monitoring.SmartSignals
+﻿namespace Microsoft.Azure.Monitoring.SmartSignals
 {
     using System.Collections.Generic;
     using System.Threading;
--- src/SDK/SmartSignalsSDK/SmartSignalDetection.cs
+++ src/SDK/SmartSignalsSDK/SmartSignalDetection.cs
@@ -1,10 +1,4 @@
-﻿//-----------------------------------------------------------------------
-// <copyright company="Microsoft Corporation">
-//        Copyright (c) Microsoft Corporation.  All rights reserved.
-// </copyright>
-//-----------------------------------------------------------------------
-
-namespace Microsoft.Azure.Monitoring.SmartSignals
+﻿namespace Microsoft.Azure.Monitoring.SmartSignals
 {
     /// <summary>
     /// A class representing a detection made by a Smart Signal.
