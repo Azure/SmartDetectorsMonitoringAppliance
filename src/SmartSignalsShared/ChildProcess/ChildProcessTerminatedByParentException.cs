beab342622b58b241c51f771c74bd9574961f8a3
Liad Tal <7619665+liadtal@users.noreply.github.com>
2017-12-06 16:57:02 +0200

    Features/lital/child process (#7)
    * Add ChildProcessManager
    
    * Fix compilation errors after merge
    
    * Fix comment
    
    * Small changes following review comments
    
    * Some fixes following code review comments
    
    * Simplify tasks management in child process
    
    * Add a test for stuck child process, and a few small fixes following comments on the PR
    
    * Rename from ChildProcessTest to TestChildProcess
    
    * Improve exception handling for listener tasks, and a few fixes following comments
    
    * * Improve stream IO
    * Send process input in a pipe instead of in command line args
    * Add exception for the case of un-graceful cancellation of the child process
    
    * Add process ID to some of the traces
    
    * * Convert the manager to a class, with interface
    * Return the process ID that was created (and kill it in the test)
    
    * Add loop when reading from the stream, and some other small fixes
    
    * Bug fix

---
+++ src/SmartSignalsShared/ChildProcess/ChildProcessTerminatedByParentException.cs
@@ -0,0 +1,33 @@
+﻿namespace Microsoft.Azure.Monitoring.SmartSignals.Shared.ChildProcess
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// This exception is thrown when a child process was forcefully terminated by the parent process.
+    /// </summary>
+    [Serializable]
+    public class ChildProcessTerminatedByParentException : ChildProcessException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChildProcessTerminatedByParentException"/> class
+        /// </summary>
+        public ChildProcessTerminatedByParentException()
+            : base("The child process failed to be cancelled - was terminated by parent")
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChildProcessTerminatedByParentException"/> class
+        /// with serialized data.
+        /// </summary>
+        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SeraizliationInfo"/> that holds the serialized
+        /// object data about the exception being thrown.</param>
+        /// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext"/> that contains contextual
+        /// information about the source or destination.</param>
+        protected ChildProcessTerminatedByParentException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
---
+++ src/SmartSignalsShared/ChildProcess/IChildProcessManager.cs
@@ -0,0 +1,60 @@
+﻿namespace Microsoft.Azure.Monitoring.SmartSignals.Shared.ChildProcess
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// An interface of a class that manages running a task in a separate process
+    /// </summary>
+    public interface IChildProcessManager
+    {
+        /// <summary>
+        /// Gets or sets the amount of time to wait on the child process till it gracefully cancels
+        /// </summary>
+        int CancellationGraceTimeInSeconds { get; set; }
+
+        /// <summary>
+        /// Gets the current status of running the child process
+        /// </summary>
+        RunChildProcessStatus CurrentStatus { get; }
+
+        /// <summary>
+        /// Gets the list of child process IDs created by this instance
+        /// </summary>
+        List<int> ChildProcessIds { get; }
+
+        /// <summary>
+        /// Runs a child process, synchronously, with the specified input.
+        /// This method should be called by the parent process to start the child process.
+        /// The child process should call <see cref="RunAndListenToParentAsync{TInput,TOutput}"/>,
+        /// providing the main method that receives the input object and returns an object of
+        /// type <see cref="TOutput"/>.
+        /// </summary>
+        /// <typeparam name="TOutput">The child process output type</typeparam>
+        /// <param name="exePath">The child process' executable file path</param>
+        /// <param name="input">The child process input</param>
+        /// <param name="tracer">The tracer</param>
+        /// <param name="cancellationToken">The cancellation token</param>
+        /// <exception cref="InvalidOperationException">The child process could not be started</exception>
+        /// <exception cref="ChildProcessException">The child process failed - see InnerException ro details</exception>
+        /// <returns>A <see cref="Task{TResult}"/>, returning the child process output</returns>
+        Task<TOutput> RunChildProcessAsync<TOutput>(string exePath, object input, ITracer tracer, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Runs the child process task. This method reads and validates the command line
+        /// arguments, starts listening to the parent process (for cancellation/termination),
+        /// runs the specified function, and returns the result to the parent process.
+        /// Should be called by the child process when it starts.
+        /// </summary>
+        /// <typeparam name="TInput">The child process input type</typeparam>
+        /// <typeparam name="TOutput">The child process output type</typeparam>
+        /// <param name="args">The command line arguments</param>
+        /// <param name="function">The function to run</param>
+        /// <param name="tracer">The tracer</param>
+        /// <exception cref="ArgumentException">The wrong number of arguments was provided</exception>
+        /// <returns>A <see cref="Task"/>, running the specified function and listening to the parent</returns>
+        Task RunAndListenToParentAsync<TInput, TOutput>(string[] args, Func<TInput, CancellationToken, Task<TOutput>> function, ITracer tracer) where TOutput : class;
+    }
+}
