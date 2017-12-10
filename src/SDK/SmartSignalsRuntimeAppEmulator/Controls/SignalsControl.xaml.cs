9d62b9b6f2b62c073d864668e11e6484de243fbe
Eran Gonen <erangon@yahoo.com>
2017-12-10 09:38:20 +0200

    Initial version of the emulator

---
+++ src/SDK/SmartSignalsRuntimeAppEmulator/Controls/SignalsControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Microsoft.Azure.Monitoring.SmartSignals.Emulator.Controls
+{
+    /// <summary>
+    /// Interaction logic for SignalsControl.xaml
+    /// </summary>
+    public partial class SignalsControl : UserControl
+    {
+        public SignalsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
