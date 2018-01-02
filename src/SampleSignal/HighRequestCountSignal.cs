3aab41583cadf1f4208f94a36d34108f8f483963
netta-schwartz <netta.s@gmail.com>
2018-01-02 17:35:24 +0200

    Added logics to the signal, moved the project folder

---
+++ src/SampleSignal/HighRequestCountSignal.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Microsoft.Azure.Monitoring.SmartSignals.SampleSignal
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Sample signal logic, tests fetching from different clients and result presentation
+    /// </summary>
+    public class HighRequestCountSignal : ISmartSignal
+
+    {
+        private const string MaximumProcessorTimeLogAnalyticsQuery =
+            "Perf " +
+            "| where TimeGenerated  >= ago(1h)" +
+            "| where CounterName == '% Processor Time'" +
+            "| summarize arg_max(CounterValue, TimeGenerated) ";
+
+        private const string HighestAppApplicationInsightsQuery =
+            "requests" +
+            "| where timestamp > ago(1d)" +
+            "| summarize countReqByAppName = count() by appName"+
+            "| summarize arg_max(countReqByAppName, appName)";
+
+
+        /// <summary>
+        /// This method tests the signal analysis services
+        /// </summary>
+        public async Task<SmartSignalResult> AnalyzeResourcesAsync(AnalysisRequest analysisRequest, ITracer tracer, CancellationToken cancellationToken)
+        {
+            SmartSignalResult smartSignalResult = new SmartSignalResult();
+            // 1. Fetch from Application Insights client - name of an application with maximum request in the last 24h
+            ITelemetryDataClient applicationInsightsDataClient = await analysisRequest.AnalysisServicesFactory.CreateApplicationInsightsTelemetryDataClientAsync(
+                analysisRequest.TargetResources, cancellationToken);
+            IList<DataTable> aiResult = await applicationInsightsDataClient.RunQueryAsync(HighestAppApplicationInsightsQuery,cancellationToken);
+            long countReqByAppName = 0;
+            string appName = "N/A";
+            if (aiResult.Count > 0)
+            {
+                countReqByAppName = Convert.ToInt64(aiResult[0].Rows[0]["countReqByAppName"]);
+                appName = Convert.ToString(aiResult[0].Rows[0]["appName"]);
+                tracer.TraceInformation($"App {appName} has high request count of {countReqByAppName}");
+            }
+            else
+            {
+                tracer.TraceError("Failed to perform the query in Application Insights");
+            }
+
+            // 2. Fetch from Log Analytics - time of highest CPU in the last 24h
+            ITelemetryDataClient logAnalyticsDataClient = await analysisRequest.AnalysisServicesFactory.CreateLogAnalyticsTelemetryDataClientAsync(
+                analysisRequest.TargetResources, cancellationToken);
+            IList<DataTable> laResult = await logAnalyticsDataClient.RunQueryAsync(MaximumProcessorTimeLogAnalyticsQuery, cancellationToken);
+            double highestProcessorTimePercent = 0;
+            DateTime timeOfHighestProcessorTime = new DateTime();
+            if (laResult.Count > 0)
+            {
+                highestProcessorTimePercent = Convert.ToDouble(laResult[0].Rows[0]["CounterValue"]);
+                timeOfHighestProcessorTime = Convert.ToDateTime(laResult[0].Rows[0]["TimeGenerated"]);
+                tracer.TraceInformation($"The highest value of % Processor Time {highestProcessorTimePercent} appeared at {timeOfHighestProcessorTime}");
+            }
+            else
+            {
+                tracer.TraceError("Failed to perform the query in Log Analytics");
+            }
+
+            analysisRequest.TargetResources.ForEach(resourceIdentifier => smartSignalResult.ResultItems.Add(
+                new HighRequestCountSignalResultItem("High Processing Time Percentage(LA) and Request Count(AI)",appName,countReqByAppName, highestProcessorTimePercent, timeOfHighestProcessorTime, resourceIdentifier)));
+            return smartSignalResult;
+        }
+    }
+}
---
+++ src/SampleSignal/HighRequestCountSignalResultItem.cs
@@ -0,0 +1,59 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="HighRequestCountSignalResultItem.cs" company="Microsoft Corporation">
+//        Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+
+namespace Microsoft.Azure.Monitoring.SmartSignals.SampleSignal
+{    
+    /// <summary>
+    /// A class representing the result of a sample signal 
+    /// </summary>
+    class HighRequestCountSignalResultItem : SmartSignalResultItem
+    {
+        // The max request count - fetched from application insights
+        [ResultItemPredicate]
+        [ResultItemPresentation(ResultItemPresentationSection.Property, "Maximum Request Count for the application", InfoBalloon = "Maximum requests for application '{AppName}'", Component = ResultItemPresentationComponent.Details | ResultItemPresentationComponent.Summary)]
+        public long RequestCount { get; set; }
+
+        // The name of the application with the maximum requests in the last day - fetched from application insights
+        [ResultItemPredicate]
+        [ResultItemPresentation(ResultItemPresentationSection.Property, "App Name", InfoBalloon = "App Name", Component = ResultItemPresentationComponent.Details | ResultItemPresentationComponent.Summary)]
+        public string AppName {get; set; }
+
+        // The highest Prcoessor Time percentage in the last hour - fetched from log analytics
+        [ResultItemPredicate]
+        [ResultItemPresentation(ResultItemPresentationSection.Property, "Highest Processor Time Percentage", InfoBalloon = "The highest Processor Time percentage", Component = ResultItemPresentationComponent.Details | ResultItemPresentationComponent.Summary)]
+        public double HighestProcessorTimePercent { get; set; }
+
+        // The time of the highest Processor Time percentage in the last hour - fetched from log analytics
+        [ResultItemPredicate]
+        [ResultItemPresentation(ResultItemPresentationSection.Property, "Highest CPU Percentage Time", InfoBalloon = "When was the highest CPU percentage?", Component = ResultItemPresentationComponent.Details | ResultItemPresentationComponent.Summary)]
+        public DateTime TimeOfHighestCpu { get; set; }
+
+        // Describe the problem
+        [ResultItemPresentation(ResultItemPresentationSection.Analysis, "What is the problem?", InfoBalloon = "The problem analysis", Component = ResultItemPresentationComponent.Details)]
+        public string Problem => "There was an unusually high request count for the application";
+
+        // Time chart to appear in the details component - fetched from Log Analytics by default
+        [ResultItemPresentation(ResultItemPresentationSection.Chart, "Time Chart", InfoBalloon = "Time Chart showing requests per app in the last day", Component = ResultItemPresentationComponent.Details)]
+        public string DetailQuery => "Perf | where TimeGenerated  >= ago(1h) | where CounterName == '% Processor Time'| project CounterValue,TimeGenerated   | render timechart ";
+
+        // Bar chart to appear in the summary component - fetched from Log Analytics by default
+        [ResultItemPresentation(ResultItemPresentationSection.Chart, "Bar Chart", InfoBalloon = "Bar Chart showing requests per app  in the last day", Component = ResultItemPresentationComponent.Summary)]
+        public string SummaryQuery => "Perf | where TimeGenerated  >= ago(1h) | where CounterName == '% Processor Time'| project CounterValue,TimeGenerated   | render barchart";
+
+        // All the data that is displayed in the charts - fetched from Log Analytics by default
+        [ResultItemPresentation(ResultItemPresentationSection.AdditionalQuery, "More Information", InfoBalloon = "More Information...", Component = ResultItemPresentationComponent.Details)]
+        public string AdditionalQuery => "Perf | where TimeGenerated  >= ago(1h) | where CounterName == '% Processor Time'| project CounterValue,TimeGenerated";
+        public HighRequestCountSignalResultItem(string title, string appName, long requestCount, double highestProcessorTimePercent, DateTime timeOfHighestCpu, ResourceIdentifier resourceIdentifier) : base(title, resourceIdentifier)
+        {
+            this.AppName = appName;
+            this.RequestCount = requestCount;
+            this.HighestProcessorTimePercent = highestProcessorTimePercent;
+            this.TimeOfHighestCpu = timeOfHighestCpu;
+        }
+    }
+}
