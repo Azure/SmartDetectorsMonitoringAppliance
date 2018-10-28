ceef175d1e93baaf3acf8209169d31c5ec0be811
bermanguy <guyber@microsoft.com>
2018-10-28 11:38:16 +0200

    support alert table prop presentation from emulator (#11)
    * support alert table prop presentation from emulator
    
    * revert change in auth services class
    
    * CR fixes

---
+++ test/SDK/MonitoringApplianceEmulatorTests/TestTableAlertPropertyValue.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TestTableAlertPropertyValue.cs" company="Microsoft Corporation">
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace MonitoringApplianceEmulatorTests
+{
+    using Microsoft.Azure.Monitoring.SmartDetectors;
+    using Microsoft.Azure.Monitoring.SmartDetectors.RuntimeEnvironment.Contracts;
+
+    /// <summary>
+    /// Illustrate a class that represents a row of a <see cref="TableAlertProperty"/> for test puspose.
+    /// </summary>
+    public class TestTableAlertPropertyValue
+    {
+        [AlertPresentationTableColumn("First Name")]
+        public string FirstName { get; set; }
+
+        [AlertPresentationTableColumn("Last Name")]
+        public string LastName { get; set; }
+
+        [AlertPresentationTableColumn("Goals avg")]
+        public double Goals { get; set; }
+    }
+}
