1c9d726f0e11525a3381cf33472cc15c5c607c82
Guy Berman <guyber@microsoft.com>
2018-11-11 11:35:43 +0200

    CR fixes - support both datetime and numeric X axis types

--- src/SDK/MonitoringApplianceEmulator/ViewModels/ChartPropertyControlViewModel.cs
+++ src/SDK/MonitoringApplianceEmulator/ViewModels/ChartPropertyControlViewModel.cs
@@ -13,11 +13,12 @@ namespace Microsoft.Azure.Monitoring.SmartDetectors.MonitoringApplianceEmulator.
     using LiveCharts;
     using LiveCharts.Configurations;
     using LiveCharts.Wpf;
+    using Microsoft.Azure.Monitoring.SmartDetectors.MonitoringApplianceEmulator.Controls;
     using Microsoft.Azure.Monitoring.SmartDetectors.MonitoringApplianceEmulator.Models;
     using Microsoft.Azure.Monitoring.SmartDetectors.RuntimeEnvironment.Contracts;
 
     /// <summary>
-    /// The view model class for the <see cref="ChartPropertyControlViewModel"/> control.
+    /// The view model class for the <see cref="ChartPropertyControl"/> control.
     /// </summary>
     public class ChartPropertyControlViewModel : ObservableObject
     {
@@ -31,66 +32,26 @@ namespace Microsoft.Azure.Monitoring.SmartDetectors.MonitoringApplianceEmulator.
         /// <param name="chartAlertProperty">The chart alert property that should be displayed.</param>
         public ChartPropertyControlViewModel(ChartAlertProperty chartAlertProperty)
         {
-            var chartValues = new ChartValues<DateTimeDataPoint>();
+            this.Title = chartAlertProperty.DisplayName;
 
-            foreach (var dataPoint in chartAlertProperty.DataPoints)
+            if (chartAlertProperty.YAxisType != ChartAxisType.Number)
             {
-                DateTime dateTime;
-                try
-                {
-                    dateTime = Convert.ToDateTime(dataPoint.X, CultureInfo.InvariantCulture);
-                }
-                catch (Exception e) when (e is FormatException || e is InvalidCastException)
-                {
-                    throw new InvalidCastException($"The data point's X value '{dataPoint.X}' is not of DateTime type", e);
-                }
-
-                double value;
-                try
-                {
-                    value = Convert.ToDouble(dataPoint.Y, CultureInfo.InvariantCulture);
-                }
-                catch (Exception e) when (e is FormatException || e is InvalidCastException || e is OverflowException)
-                {
-                    throw new InvalidCastException($"The data point's Y value '{dataPoint.Y}' is not of a numeric type", e);
-                }
-
-                chartValues.Add(new DateTimeDataPoint(dateTime, value));
+                throw new InvalidOperationException($"Charts with Y axis type other than {ChartAxisType.Number} are not supported.");
             }
 
-            /*
-             * In order to support
-             * Since we are using DateTime.Ticks as X, the width of the bar is 1 tick and 1 tick is 1 millisecond.
-             * In order to make our bars visible we need to change the unit of the chart. For the initial view are going to use hours.
-             */
-            double xAxisFactor = chartValues.Skip(1).First().DateTime.Ticks - chartValues.First().DateTime.Ticks;
-
-            CartesianMapper<DateTimeDataPoint> pointMapperConfig = Mappers.Xy<DateTimeDataPoint>()
-                .X(dateTimeDataPoint => dateTimeDataPoint.DateTime.Ticks / xAxisFactor)
-                .Y(dateTimeDataPoint => dateTimeDataPoint.Value);
-
-            this.SeriesCollection = new SeriesCollection(pointMapperConfig);
-
-            if (chartAlertProperty.ChartType == ChartType.LineChart)
+            switch (chartAlertProperty.XAxisType)
             {
-                this.SeriesCollection.Add(new LineSeries
-                {
-                    Values = chartValues,
-                    Stroke = SeriesColor,
-                    Fill = Brushes.Transparent
-                });
+                case ChartAxisType.Date:
+                    this.InitializeDateTimeXAxisChart(chartAlertProperty);
+                    break;
+                case ChartAxisType.Number:
+                    this.InitializeNumberXAxisChart(chartAlertProperty);
+                    break;
+                case ChartAxisType.String:
+                    throw new NotImplementedException($"Charts with {ChartAxisType.String} X axis type will be supported soon.");
+                default:
+                    throw new InvalidOperationException($"Charts with X axis type {chartAlertProperty.XAxisType} are not supported.");
             }
-            else
-            {
-                this.SeriesCollection.Add(new ColumnSeries
-                {
-                    Values = chartValues,
-                    Stroke = SeriesColor,
-                    Fill = SeriesColor
-                });
-            }
-
-            this.XAxisFormatter = value => new DateTime((long)(value * xAxisFactor)).ToString(CultureInfo.InvariantCulture);
         }
 
         /// <summary>
@@ -111,8 +72,133 @@ namespace Microsoft.Azure.Monitoring.SmartDetectors.MonitoringApplianceEmulator.
         }
 
         /// <summary>
-        /// Gets the series collection.
+        /// Gets or sets the series collection.
         /// </summary>
-        public Func<double, string> XAxisFormatter { get; }
+        public Func<double, string> XAxisFormatter { get; set; }
+
+        /// <summary>
+        /// Gets the chart title.
+        /// </summary>
+        public string Title { get; }
+
+        /// <summary>
+        /// Converts a point's coordinate value to double.
+        /// </summary>
+        /// <param name="coordinateValue">The chart alert property that should be displayed.</param>
+        /// <param name="coordinateName">The coordinate name (e.g. X, Y).</param>
+        /// <returns>The coordinate value as double</returns>
+        private static double ConvertCordinateValueToDouble(object coordinateValue, string coordinateName)
+        {
+            try
+            {
+                return Convert.ToDouble(coordinateValue, CultureInfo.InvariantCulture);
+            }
+            catch (Exception e) when (e is FormatException || e is InvalidCastException || e is OverflowException)
+            {
+                throw new InvalidCastException($"The data point's {coordinateName} value - '{coordinateValue}', is not of a numeric type", e);
+            }
+        }
+
+        /// <summary>
+        /// Initializes a chart with X axis from type <see cref="SmartDetectors.ChartAxisType.DateAxis"/>.
+        /// </summary>
+        /// <param name="chartAlertProperty">The chart alert property that should be displayed.</param>
+        private void InitializeDateTimeXAxisChart(ChartAlertProperty chartAlertProperty)
+        {
+            var chartValues = new ChartValues<ChartDataPoint<DateTime>>();
+            var sortedDateTimePoints = chartAlertProperty.DataPoints.OrderBy(point => point.X);
+            foreach (var dataPoint in sortedDateTimePoints)
+            {
+                DateTime x;
+                try
+                {
+                    x = Convert.ToDateTime(dataPoint.X, CultureInfo.InvariantCulture);
+                }
+                catch (Exception e) when (e is FormatException || e is InvalidCastException)
+                {
+                    throw new InvalidCastException($"The data point's X value '{dataPoint.X}' is not of DateTime type", e);
+                }
+
+                double y = ConvertCordinateValueToDouble(dataPoint.Y, "Y");
+
+                chartValues.Add(new ChartDataPoint<DateTime>(x, y));
+            }
+
+             // In order to support Bar Chart, Since we are using DateTime.Ticks as X, the width of the bar is 1 tick and 1 tick is 1 millisecond.
+             // In order to make our bars visible we need to change the unit of the chart. Hence, we are taking the difference between 2 first points (assuming difference between every 2 points is equal)
+            double xAxisFactor = chartValues.Skip(1).First().X.Ticks - chartValues.First().X.Ticks;
+
+            CartesianMapper<ChartDataPoint<DateTime>> pointMapperConfig = Mappers.Xy<ChartDataPoint<DateTime>>()
+                .X(dateTimeDataPoint => dateTimeDataPoint.X.Ticks / xAxisFactor)
+                .Y(dateTimeDataPoint => dateTimeDataPoint.Y);
+
+            this.SeriesCollection = new SeriesCollection(pointMapperConfig);
+
+            this.XAxisFormatter = value => new DateTime((long)(value * xAxisFactor)).ToString(CultureInfo.InvariantCulture);
+
+            if (chartAlertProperty.ChartType == ChartType.LineChart)
+            {
+                this.SeriesCollection.Add(new LineSeries
+                {
+                    Values = chartValues,
+                    Stroke = SeriesColor,
+                    Fill = Brushes.Transparent
+                });
+            }
+            else
+            {
+                this.SeriesCollection.Add(new ColumnSeries
+                {
+                    Values = chartValues,
+                    Stroke = SeriesColor,
+                    Fill = SeriesColor
+                });
+            }
+        }
+
+        /// <summary>
+        /// Initializes a chart with X axis from type <see cref="SmartDetectors.ChartAxisType.NumberAxis"/>.
+        /// </summary>
+        /// <param name="chartAlertProperty">The chart alert property that should be displayed.</param>
+        private void InitializeNumberXAxisChart(ChartAlertProperty chartAlertProperty)
+        {
+            var chartValues = new ChartValues<ChartDataPoint<double>>();
+            var sortedNumericPoints = chartAlertProperty.DataPoints.OrderBy(point => point.X);
+
+            foreach (var dataPoint in sortedNumericPoints)
+            {
+                double x = ConvertCordinateValueToDouble(dataPoint.X, "X");
+                double y = ConvertCordinateValueToDouble(dataPoint.Y, "Y");
+
+                chartValues.Add(new ChartDataPoint<double>(x, y));
+            }
+
+            CartesianMapper<ChartDataPoint<double>> pointMapperConfig = Mappers.Xy<ChartDataPoint<double>>()
+                .X(dateTimeDataPoint => dateTimeDataPoint.X)
+                .Y(dateTimeDataPoint => dateTimeDataPoint.Y);
+
+            this.SeriesCollection = new SeriesCollection(pointMapperConfig);
+
+            this.XAxisFormatter = value => value.ToString(CultureInfo.InvariantCulture);
+
+            if (chartAlertProperty.ChartType == ChartType.LineChart)
+            {
+                this.SeriesCollection.Add(new LineSeries
+                {
+                    Values = chartValues,
+                    Stroke = SeriesColor,
+                    Fill = Brushes.Transparent
+                });
+            }
+            else
+            {
+                this.SeriesCollection.Add(new ColumnSeries
+                {
+                    Values = chartValues,
+                    Stroke = SeriesColor,
+                    Fill = SeriesColor
+                });
+            }
+        }
     }
 }
---
+++ test/SDK/MonitoringApplianceEmulatorTests/ViewModels/ChartPropertyControlViewModelTests.cs
@@ -0,0 +1,119 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ChartPropertyControlViewModelTests.cs" company="Microsoft Corporation">
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace MonitoringApplianceEmulatorTests.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using LiveCharts.Wpf;
+    using Microsoft.Azure.Monitoring.SmartDetectors.MonitoringApplianceEmulator.ViewModels;
+    using Microsoft.Azure.Monitoring.SmartDetectors.RuntimeEnvironment.Contracts;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ChartPropertyControlViewModelTests
+    {
+        private static readonly DateTime Now = DateTime.Now;
+
+        private static readonly List<ChartPoint> DateTimeChartPoints = new List<ChartPoint>
+        {
+            new ChartPoint(Now, 8.0),
+            new ChartPoint(Now.AddDays(1), 6.0),
+            new ChartPoint(Now.AddDays(2), 4.0),
+            new ChartPoint(Now.AddDays(3), 14.0),
+            new ChartPoint(Now.AddDays(4), 10.0)
+        };
+
+        private static readonly List<ChartPoint> NumericChartPoints = new List<ChartPoint>
+        {
+            new ChartPoint(1.0, 8.0),
+            new ChartPoint(2.0, 6.0),
+            new ChartPoint(3.0, 4.0),
+            new ChartPoint(4.0, 14.0),
+            new ChartPoint(5.0, 10.0)
+        };
+
+        public delegate void ChartPointsAssertionDelegate(List<ChartPoint> expectedPoints, List<LiveCharts.ChartPoint> actualPoints);
+
+        [TestMethod]
+        public void WhenCreatingNewViewModelForLineChartWithDateTimeXAxisTypeThenItWasInitializedCorrectly()
+        {
+            AssertChartViewModel<LineSeries>(ChartType.LineChart, ChartAxisType.Date, DateTimeChartPoints, AssertDateTimeChartPoints);
+        }
+
+        [TestMethod]
+        public void WhenCreatingNewViewModelForBarChartWithDateTimeXAxisTypeThenItWasInitializedCorrectly()
+        {
+            AssertChartViewModel<ColumnSeries>(ChartType.BarChart, ChartAxisType.Date, DateTimeChartPoints, AssertDateTimeChartPoints);
+        }
+
+        [TestMethod]
+        public void WhenCreatingNewViewModelForLineChartWithNumericXAxisTypeThenItWasInitializedCorrectly()
+        {
+            AssertChartViewModel<LineSeries>(ChartType.LineChart, ChartAxisType.Number, NumericChartPoints, AssertNumericChartPoints);
+        }
+
+        [TestMethod]
+        public void WhenCreatingNewViewModelForBarChartWithNumericXAxisTypeThenItWasInitializedCorrectly()
+        {
+            AssertChartViewModel<ColumnSeries>(ChartType.BarChart, ChartAxisType.Number, NumericChartPoints, AssertNumericChartPoints);
+        }
+
+        private static void AssertChartViewModel<T>(ChartType chartType, ChartAxisType xAxisType, List<ChartPoint> expectedChartPoints, ChartPointsAssertionDelegate pointsAssertionMethod)
+            where T : Series
+        {
+            // Init
+            var chartAlertProperty = new ChartAlertProperty(
+                "propertyName",
+                "displayName",
+                1,
+                chartType,
+                xAxisType,
+                ChartAxisType.Number,
+                expectedChartPoints);
+
+            // Act
+            var chartPropertyControlViewModel = new ChartPropertyControlViewModel(chartAlertProperty);
+
+            // Assert generated Line Series
+            Assert.IsNotNull(chartPropertyControlViewModel);
+            Assert.IsNotNull(chartPropertyControlViewModel.SeriesCollection[0]);
+
+            T series = chartPropertyControlViewModel.SeriesCollection[0] as T;
+            Assert.IsNotNull(series);
+
+            List<LiveCharts.ChartPoint> actualDataPoints = series.ChartPoints.ToList();
+            pointsAssertionMethod(expectedChartPoints, actualDataPoints);
+        }
+
+        private static void AssertDateTimeChartPoints(List<ChartPoint> expectedChartPoints, List<LiveCharts.ChartPoint> actualDataPoints)
+        {
+            double xAxisFactor = Now.AddDays(1).Ticks - Now.Ticks;
+
+            for (var i = 0; i < actualDataPoints.Count(); i++)
+            {
+                var expectedDataPoint = expectedChartPoints[i];
+                var actualDataPoint = actualDataPoints[i];
+
+                Assert.AreEqual((double)((DateTime)expectedDataPoint.X).Ticks / xAxisFactor, actualDataPoint.X);
+                Assert.AreEqual(expectedDataPoint.Y, actualDataPoint.Y);
+            }
+        }
+
+        private static void AssertNumericChartPoints(List<ChartPoint> expectedChartPoints, List<LiveCharts.ChartPoint> actualDataPoints)
+        {
+            for (var i = 0; i < actualDataPoints.Count(); i++)
+            {
+                var expectedDataPoint = expectedChartPoints[i];
+                var actualDataPoint = actualDataPoints[i];
+
+                Assert.AreEqual(expectedDataPoint.X, actualDataPoint.X);
+                Assert.AreEqual(expectedDataPoint.Y, actualDataPoint.Y);
+            }
+        }
+    }
+}
