a084bbaf0aea4ef3c7855639a7f6019d5ab8af29
Ilya Aizin <ilaizi@microsoft.com>
2018-11-18 12:16:34 +0200

    Change array to list to avoid weird serialization issues

--- src/SDK/SmartDetectors.Extensions/Presentation/AlertExtensions.cs
+++ src/SDK/SmartDetectors.Extensions/Presentation/AlertExtensions.cs
@@ -310,12 +310,12 @@ namespace Microsoft.Azure.Monitoring.SmartDetectors.Presentation
             AlertPresentationTableAttribute tableAttribute)
         {
             var columns = new List<TableColumn>();
-            var rows = new Dictionary<string, string>[tableRows.Count];
+            var rows = new List<Dictionary<string, string>>();
 
             // Initialize the table rows with new dictionaries
             for (int i = 0; i < tableRows.Count; i++)
             {
-                rows[i] = new Dictionary<string, string>();
+                rows.Add(new Dictionary<string, string>());
             }
 
             // We scan the table by columns to we'll handle a single property at a time
