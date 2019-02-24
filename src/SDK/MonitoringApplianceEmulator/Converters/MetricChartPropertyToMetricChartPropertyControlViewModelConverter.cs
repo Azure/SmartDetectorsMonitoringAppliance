57cd8fcf90a6ca1c07fb3f2208e3f83bbedb2fd2
Liad Tal <lital@microsoft.com>
2019-02-24 13:01:28 +0200

    Add MetricChartPropertyControlViewModel - no implementation yet

---
+++ src/SDK/MonitoringApplianceEmulator/Converters/MetricChartPropertyToMetricChartPropertyControlViewModelConverter.cs
@@ -0,0 +1,66 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MetricChartPropertyToMetricChartPropertyControlViewModelConverter.cs" company="Microsoft Corporation">
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Microsoft.Azure.Monitoring.SmartDetectors.MonitoringApplianceEmulator.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+    using Microsoft.Azure.Monitoring.SmartDetectors.MonitoringApplianceEmulator.ViewModels;
+    using Microsoft.Azure.Monitoring.SmartDetectors.RuntimeEnvironment.Contracts.AlertProperties;
+
+    /// <summary>
+    /// Implementation of <see cref="IValueConverter"/> for converting from a <see cref="ChartAlertProperty"/> value to <see cref="ChartPropertyControlViewModel"/>.
+    /// </summary>
+    public class MetricChartPropertyToMetricChartPropertyControlViewModelConverter : IValueConverter
+    {
+        #region Implementation of IValueConverter
+
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // This 'if' clause was added to workaround a known issue in WPF -
+            // DataContext of a 'ListViewItem' is being assigned with '{DisconnectedItem}' object and then a null value whenever its container is closed.
+            if (value == null || value.ToString() == "{DisconnectedItem}")
+            {
+                return value;
+            }
+
+            if (value is MetricChartAlertProperty metricChartAlertProperty)
+            {
+                return new MetricChartPropertyControlViewModel(metricChartAlertProperty);
+            }
+
+            throw new ArgumentException($"The value parameter must be of type {nameof(MetricChartAlertProperty)} or {nameof(ChartPropertyControlViewModel)}, but it is from type {value.GetType().Name}.", nameof(value));
+        }
+
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        /// <param name="value">The value that is produced by the binding target.</param>
+        /// <param name="targetType">The type to convert to.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ src/SDK/MonitoringApplianceEmulator/ViewModels/MetricChartPropertyControlViewModel.cs
@@ -0,0 +1,65 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MetricChartPropertyControlViewModel.cs" company="Microsoft Corporation">
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Microsoft.Azure.Monitoring.SmartDetectors.MonitoringApplianceEmulator.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Threading;
+    using System.Windows.Media;
+    using LiveCharts;
+    using LiveCharts.Configurations;
+    using LiveCharts.Wpf;
+    using Microsoft.Azure.Monitoring.SmartDetectors.Metric;
+    using Microsoft.Azure.Monitoring.SmartDetectors.MonitoringApplianceEmulator.Controls;
+    using Microsoft.Azure.Monitoring.SmartDetectors.MonitoringApplianceEmulator.Models;
+    using Microsoft.Azure.Monitoring.SmartDetectors.RuntimeEnvironment.Contracts.AlertProperties;
+    using Unity;
+    using ChartPoint = Microsoft.Azure.Monitoring.SmartDetectors.RuntimeEnvironment.Contracts.AlertProperties.ChartPoint;
+
+    /// <summary>
+    /// The view model class for the <see cref="ChartPropertyControl"/> control.
+    /// </summary>
+    public class MetricChartPropertyControlViewModel : ObservableObject
+    {
+        private SeriesCollection seriesCollection;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MetricChartPropertyControlViewModel"/> class.
+        /// </summary>
+        /// <param name="metricChartAlertProperty">The chart alert properties container that should be displayed.</param>
+        public MetricChartPropertyControlViewModel(MetricChartAlertProperty metricChartAlertProperty)
+        {
+            this.Title = metricChartAlertProperty.DisplayName;
+
+            // TODO: Implement charts
+        }
+
+        /// <summary>
+        /// Gets the chart title.
+        /// </summary>
+        public string Title { get; }
+
+        /// <summary>
+        /// Gets the series collection.
+        /// </summary>
+        public SeriesCollection SeriesCollection
+        {
+            get
+            {
+                return this.seriesCollection;
+            }
+
+            private set
+            {
+                this.seriesCollection = value;
+                this.OnPropertyChanged();
+            }
+        }
+    }
+}
