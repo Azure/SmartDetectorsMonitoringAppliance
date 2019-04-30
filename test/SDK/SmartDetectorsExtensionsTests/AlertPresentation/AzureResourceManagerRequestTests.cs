0307a8e978146936669887c2e9161dffb8e5d5d4
Eran Gonen <erang@outlook.com>
2019-04-30 09:16:19 +0300

    Added support for ARM request alert properties (#61)
    * Added ARM request alert property
    
    * Moved the extensions tests to the proper place
    
    * Moved the AlertPresentationTests class to the extensions test project
    
    * Split the presentation attribute tests to seperate test classes
    
    * Beefed up tests
    
    * Updated the nuget versions
    
    * Fixed CR comments

---
+++ test/SDK/SmartDetectorsExtensionsTests/AlertPresentation/AzureResourceManagerRequestTests.cs
@@ -0,0 +1,114 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AzureResourceManagerRequestTests.cs" company="Microsoft Corporation">
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace SmartDetectorsExtensionsTests.AlertPresentation
+{
+    using System;
+    using Microsoft.Azure.Monitoring.SmartDetectors;
+    using Microsoft.Azure.Monitoring.SmartDetectors.AlertPresentation;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class AzureResourceManagerRequestTests
+    {
+        [TestMethod]
+        public void WhenCreatingAzureResourceManagerRequestWithUriThenRequestIsCreated()
+        {
+            var armRequest = new TestAzureResourceManagerRequest(new Uri("/some/request/path", UriKind.Relative));
+            Assert.AreEqual("/some/request/path", armRequest.RequestUri.ToString());
+        }
+
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void WhenCreatingAzureResourceManagerRequestWithNullUriThenExceptionIsThrown()
+        {
+            _ = new TestAzureResourceManagerRequest(null);
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void WhenCreatingAzureResourceManagerRequestWithAbsoluteUriThenExceptionIsThrown()
+        {
+            _ = new TestAzureResourceManagerRequest(new Uri("https://www.microsoft.com"));
+        }
+
+        [TestMethod]
+        public void WhenCreatingAzureResourceManagerRequestWithResourceIdAndSuffixThenRequestIsCreated()
+        {
+            var resource = new ResourceIdentifier(ResourceType.AzureStorage, "subscription", "resourceGroup", "resourceName");
+
+            var nullSuffixRequest = new TestAzureResourceManagerRequest(resource, null);
+            Assert.AreEqual("/subscriptions/subscription/resourceGroups/resourceGroup/providers/Microsoft.Storage/storageAccounts/resourceName", nullSuffixRequest.RequestUri.ToString(), "Wrong RequestUri for nullSuffixRequest");
+
+            var emptySuffixRequest = new TestAzureResourceManagerRequest(resource, null);
+            Assert.AreEqual("/subscriptions/subscription/resourceGroups/resourceGroup/providers/Microsoft.Storage/storageAccounts/resourceName", emptySuffixRequest.RequestUri.ToString(), "Wrong RequestUri for emptySuffixRequest");
+
+            var pathSuffixRequest = new TestAzureResourceManagerRequest(resource, "/some/path");
+            Assert.AreEqual("/subscriptions/subscription/resourceGroups/resourceGroup/providers/Microsoft.Storage/storageAccounts/resourceName/some/path", pathSuffixRequest.RequestUri.ToString(), "Wrong RequestUri for pathSuffixRequest");
+
+            var noSlashPathSuffixRequest = new TestAzureResourceManagerRequest(resource, "some/path");
+            Assert.AreEqual("/subscriptions/subscription/resourceGroups/resourceGroup/providers/Microsoft.Storage/storageAccounts/resourceName/some/path", noSlashPathSuffixRequest.RequestUri.ToString(), "Wrong RequestUri for noSlashPathSuffixRequest");
+
+            var querySuffixRequest = new TestAzureResourceManagerRequest(resource, "?param=value");
+            Assert.AreEqual("/subscriptions/subscription/resourceGroups/resourceGroup/providers/Microsoft.Storage/storageAccounts/resourceName?param=value", querySuffixRequest.RequestUri.ToString(), "Wrong RequestUri for querySuffixRequest");
+        }
+
+        [TestMethod]
+        public void WhenCreatingAzureResourceManagerRequestWithResourceIdAndQueryThenRequestIsCreated()
+        {
+            var resource = new ResourceIdentifier(ResourceType.AzureStorage, "subscription", "resourceGroup", "resourceName");
+            var aiResource = new ResourceIdentifier(ResourceType.ApplicationInsights, "subscription", "resourceGroup", "resourceName");
+
+            var queryRequest = new TestAzureResourceManagerRequest(resource, "query", TimeSpan.FromHours(5));
+            Assert.AreEqual("/subscriptions/subscription/resourceGroups/resourceGroup/providers/Microsoft.Storage/storageAccounts/resourceName/providers/microsoft.insights/logs?query=query&timespan=PT5H&api-version=2018-03-01-preview", queryRequest.RequestUri.ToString(), "Wrong RequestUri for queryRequest");
+
+            var queryNoTimeSpanRequest = new TestAzureResourceManagerRequest(resource, "query", null);
+            Assert.AreEqual("/subscriptions/subscription/resourceGroups/resourceGroup/providers/Microsoft.Storage/storageAccounts/resourceName/providers/microsoft.insights/logs?query=query&api-version=2018-03-01-preview", queryNoTimeSpanRequest.RequestUri.ToString(), "Wrong RequestUri for queryNoTimeSpanRequest");
+
+            var aiQueryRequest = new TestAzureResourceManagerRequest(aiResource, "query", TimeSpan.FromHours(5));
+            Assert.AreEqual("/subscriptions/subscription/resourceGroups/resourceGroup/providers/Microsoft.Insights/components/resourceName/api/query?query=query&timespan=PT5H&api-version=2018-04-20", aiQueryRequest.RequestUri.ToString(), "Wrong RequestUri for aiQueryRequest");
+
+            var aiQueryNoTimeSpanRequest = new TestAzureResourceManagerRequest(aiResource, "query", null);
+            Assert.AreEqual("/subscriptions/subscription/resourceGroups/resourceGroup/providers/Microsoft.Insights/components/resourceName/api/query?query=query&api-version=2018-04-20", aiQueryNoTimeSpanRequest.RequestUri.ToString(), "Wrong RequestUri for aiQueryNoTimeSpanRequest");
+        }
+
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void WhenCreatingAzureResourceManagerRequestWithResourceIdAndNullQueryThenExceptionIsThrown()
+        {
+            var resource = new ResourceIdentifier(ResourceType.AzureStorage, "subscription", "resourceGroup", "resourceName");
+
+            _ = new TestAzureResourceManagerRequest(resource, null, null);
+        }
+
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void WhenCreatingAzureResourceManagerRequestWithResourceIdAndEmptyQueryThenExceptionIsThrown()
+        {
+            var resource = new ResourceIdentifier(ResourceType.AzureStorage, "subscription", "resourceGroup", "resourceName");
+
+            _ = new TestAzureResourceManagerRequest(resource, string.Empty, null);
+        }
+
+        private class TestAzureResourceManagerRequest : AzureResourceManagerRequest
+        {
+            public TestAzureResourceManagerRequest(Uri requestUri)
+                : base(requestUri)
+            {
+            }
+
+            public TestAzureResourceManagerRequest(ResourceIdentifier resource, string requestSuffix)
+                : base(resource, requestSuffix)
+            {
+            }
+
+            public TestAzureResourceManagerRequest(ResourceIdentifier resource, string query, TimeSpan? queryTimeSpan)
+                : base(resource, query, queryTimeSpan)
+            {
+            }
+        }
+    }
+}
---
+++ test/SDK/SmartDetectorsExtensionsTests/AlertPresentation/ListPropertyAttributeTests.cs
@@ -0,0 +1,111 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ListPropertyAttributeTests.cs" company="Microsoft Corporation">
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace SmartDetectorsExtensionsTests.AlertPresentation
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.Azure.Monitoring.SmartDetectors.AlertPresentation;
+    using Microsoft.Azure.Monitoring.SmartDetectors.RuntimeEnvironment.Contracts.AlertProperties;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ContractsAlert = Microsoft.Azure.Monitoring.SmartDetectors.RuntimeEnvironment.Contracts.Alert;
+
+    [TestClass]
+    public class ListPropertyAttributeTests : PresentationAttributeTestsBase
+    {
+        [TestMethod]
+        public void WhenCreatingContractsAlertThenListPropertiesAreConvertedCorrectly()
+        {
+            ContractsAlert contractsAlert = CreateContractsAlert<TestAlert>();
+
+            Assert.AreEqual(11, contractsAlert.AlertProperties.Count);
+
+            int propertyIndex = 0;
+            this.VerifyRawAlertProperty(contractsAlert.AlertProperties[propertyIndex++], "AdditionalData_0_RawProperty", "raw");
+            this.VerifyTextAlertProperty(contractsAlert.AlertProperties[propertyIndex++], "AdditionalData_0_Name1", "First name title", 0, "First name");
+            this.VerifyTextAlertProperty(contractsAlert.AlertProperties[propertyIndex++], "AdditionalData_0_Uri1", "First link", 1, "<a href=\"https://xkcd.com/\" target=\"_blank\">Link to data 1</a>");
+            this.VerifyTextAlertProperty(contractsAlert.AlertProperties[propertyIndex++], "AdditionalData_1_Name2", "Second name title", 2, "Second name");
+            this.VerifyTextAlertProperty(contractsAlert.AlertProperties[propertyIndex++], "AdditionalData_1_Uri2", "Second link", 3, "<a href=\"https://darwinawards.com/\" target=\"_blank\">Link to data 2</a>");
+            this.VerifyRawAlertProperty(contractsAlert.AlertProperties[propertyIndex++], "AdditionalData_1_MoreData_0_RawProperty", "raw");
+            this.VerifyTextAlertProperty(contractsAlert.AlertProperties[propertyIndex++], "AdditionalData_1_MoreData_0_Name1", "First name title", 4, "First name");
+            this.VerifyTextAlertProperty(contractsAlert.AlertProperties[propertyIndex++], "AdditionalData_1_MoreData_0_Uri1", "First link", 5, "<a href=\"https://xkcd.com/\" target=\"_blank\">Link to data 1</a>");
+            this.VerifyRawAlertProperty(contractsAlert.AlertProperties[propertyIndex++], "AdditionalData_1_MoreData_1_RawProperty", "raw");
+            this.VerifyTextAlertProperty(contractsAlert.AlertProperties[propertyIndex++], "AdditionalData_1_MoreData_1_Name1", "First name title", 6, "First name");
+            this.VerifyTextAlertProperty(contractsAlert.AlertProperties[propertyIndex++], "AdditionalData_1_MoreData_1_Uri1", "First link", 7, "<a href=\"https://xkcd.com/\" target=\"_blank\">Link to data 1</a>");
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void WhenCreatingContractsAlertWithInvalidListPropertyThenExceptionIsThrown()
+        {
+            ContractsAlert contractsAlert = CreateContractsAlert<TestAlertWithInvalidProperty>();
+        }
+
+        private void VerifyTextAlertProperty(AlertProperty alertProperty, string propertyName, string displayName, byte order, string value)
+        {
+            Assert.AreEqual(propertyName, alertProperty.PropertyName);
+            Assert.AreEqual(AlertPropertyType.Text, alertProperty.Type);
+            Assert.AreEqual(displayName, ((TextAlertProperty)alertProperty).DisplayName);
+            Assert.AreEqual(order, ((TextAlertProperty)alertProperty).Order);
+            Assert.AreEqual(value, ((TextAlertProperty)alertProperty).Value);
+        }
+
+        private void VerifyRawAlertProperty(AlertProperty alertProperty, string propertyName, object value)
+        {
+            Assert.AreEqual(propertyName, alertProperty.PropertyName);
+            Assert.AreEqual(AlertPropertyType.Raw, alertProperty.Type);
+            Assert.AreEqual(value, ((RawAlertProperty)alertProperty).Value);
+        }
+
+        public class TestAlert : TestAlertBase
+        {
+            [ListProperty]
+            public IList<object> AdditionalData => new List<object>()
+            {
+                new ListData1(),
+                new ListData2()
+            };
+        }
+
+        public class TestAlertWithInvalidProperty : TestAlertBase
+        {
+            [ListProperty]
+            public string NotAList => "Oops";
+        }
+
+        private class ListData1
+        {
+            [TextProperty("First name title", Order = 1)]
+            public string Name1 => "First name";
+
+            [UrlFormatter("Link to data 1")]
+            [TextProperty("First link", Order = 2)]
+            public Uri Uri1 => new Uri("https://xkcd.com/");
+
+            public string RawProperty => "raw";
+        }
+
+        private class ListData2
+        {
+            [TextProperty("Second name title", Order = 1)]
+            public string Name2 => "Second name";
+
+            [UrlFormatter("Link to data 2")]
+            [TextProperty("Second link", Order = 2)]
+            public Uri Uri2 => new Uri("https://darwinawards.com/");
+
+            [ListProperty(Order = 3)]
+            public IList<ListData1> MoreData => new List<ListData1>()
+            {
+                new ListData1(),
+                new ListData1()
+            };
+
+            [ListProperty(Order = 4)]
+            public IList<ListData1> EmptyList => new List<ListData1>();
+        }
+    }
+}
