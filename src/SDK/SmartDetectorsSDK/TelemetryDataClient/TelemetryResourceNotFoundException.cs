1bc724f61047f99aec487416f3a696b9f223c7b3
Eran Gonen <erang@microsoft.com>
2019-05-20 10:44:20 +0300

    Fixed typo in class name

--- src/SDK/SmartDetectorsSDK/TelemetryDataClient/TelemetryResourceNotFoundException.cs
+++ src/SDK/SmartDetectorsSDK/TelemetryDataClient/TelemetryResourceNotFoundException.cs
@@ -1,5 +1,5 @@
 ﻿//-----------------------------------------------------------------------
-// <copyright file="TelemetryReourceNotFoundException.cs" company="Microsoft Corporation">
+// <copyright file="TelemetryResourceNotFoundException.cs" company="Microsoft Corporation">
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // </copyright>
 //-----------------------------------------------------------------------
@@ -12,44 +12,44 @@ namespace Microsoft.Azure.Monitoring.SmartDetectors
     /// An exception indicating the telemetry resource was not found
     /// </summary>
     [Serializable]
-    public class TelemetryReourceNotFoundException : TelemetryDataClientException
+    public class TelemetryResourceNotFoundException : TelemetryDataClientException
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="TelemetryReourceNotFoundException"/> class.
+        /// Initializes a new instance of the <see cref="TelemetryResourceNotFoundException"/> class.
         /// </summary>
-        public TelemetryReourceNotFoundException()
+        public TelemetryResourceNotFoundException()
             : base(null)
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="TelemetryReourceNotFoundException"/> class.
+        /// Initializes a new instance of the <see cref="TelemetryResourceNotFoundException"/> class.
         /// </summary>
         /// <param name="message">The exception message.</param>
-        public TelemetryReourceNotFoundException(string message)
+        public TelemetryResourceNotFoundException(string message)
             : base(message)
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="TelemetryReourceNotFoundException"/> class
+        /// Initializes a new instance of the <see cref="TelemetryResourceNotFoundException"/> class
         /// </summary>
         /// <param name="message">The exception message</param>
         /// <param name="innerException">The actual exception that was thrown when saving state</param>
-        public TelemetryReourceNotFoundException(string message, Exception innerException)
+        public TelemetryResourceNotFoundException(string message, Exception innerException)
             : base(message, innerException)
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="TelemetryReourceNotFoundException"/> class
+        /// Initializes a new instance of the <see cref="TelemetryResourceNotFoundException"/> class
         /// with serialized data.
         /// </summary>
         /// <param name="info">The <see cref="SerializationInfo"/> that holds the serialized
         /// object data about the exception being thrown.</param>
         /// <param name="context">The <see cref="StreamingContext"/> that contains contextual
         /// information about the source or destination.</param>
-        protected TelemetryReourceNotFoundException(SerializationInfo info, StreamingContext context)
+        protected TelemetryResourceNotFoundException(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
         }
